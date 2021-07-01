eb70e58124760d0abb809d0a09304b24bb561736
Joe <joe45moses@gmail.com>
2021-07-01 16:12:36 +0100

    removed unused helper

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -11,15 +11,4 @@ function truncate_string($string, $length = 200): string
     return \Illuminate\Support\Str::limit($string, $length, '...');
 }
 
-/**
- * Get a star rating from a 0-100 rating
- *
- * @param int $percent
- * @return float|int
- */
-function star_rating($percent)
-{
-    return ($percent) / 20;
-}
-
 ?>
